b57a5bc5d84525f5b2fcc0184f9ad2165959bfcb
Antony Ho <ntonyworkshop@gmail.com>
2017-09-04 01:52:50 +0800

    Not specifying cert and key path in code

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ func main() {
 	accessToken := os.Getenv("ACCESS_TOKEN")
 	verifyToken := os.Getenv("VERIFY_TOKEN")
 	apiSecret := os.Getenv("API_SECRET")
+	certPath := os.Getenv("CERT_PATH")
+	keyPath := os.Getenv("key_PATH")
 	bot := mbotapi.NewBotAPI(accessToken, verifyToken, apiSecret)
 	callbacks, mux := bot.SetWebhook("/webhook")
-	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "privkey.pem", mux)
+	go http.ListenAndServeTLS("0.0.0.0:8443", certPath, keyPath, mux)
 
 	for callback := range callbacks {
 		respMsg := fmt.Sprintf("You've said: %s", callback.Message.Text)
